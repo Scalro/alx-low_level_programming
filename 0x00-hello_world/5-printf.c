9104ca31d6941c532f15c17861959673b78dc1d0
Scalro <rotichcaleb200@gmail.com>
2023-03-16 09:32:00 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - display the main function which is the entry point of a program
+ * Return: it return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
