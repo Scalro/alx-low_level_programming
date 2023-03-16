f3df152380cbdca343247f50aa9f8f45ee1f3caa
Scalro <rotichcaleb200@gmail.com>
2023-03-16 10:08:51 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is a main function
+ * Return: this returns 0 successfully
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", sizeof(int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
