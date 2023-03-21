0122160b165eb9f501780b4b7a6e500c962e95f0
Scalro <rotichcaleb200@gmail.com>
2023-03-21 10:19:18 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - main function
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+	_putchar('\n');
+}
