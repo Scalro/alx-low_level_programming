424665b2b1d982382a2249a22f01022a1759c82d
Scalro <rotichcaleb200@gmail.com>
2023-03-21 19:47:02 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - function that prints alphabets
+ * 0c: parameter to be printed
+ * Return: 1 if it is lower case
+ * and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
