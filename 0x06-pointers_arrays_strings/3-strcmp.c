1dfefb646286b072f7abeb1c82b9e373fdb4cab2
Scalro <rotichcaleb200@gmail.com>
2023-03-30 23:43:20 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string to be compared
+ * @s2: string to compare
+ * Return: returns the difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+		;
+	return (s1[i] - s2[i]);
+}
