a784832eeb0f6c9d4d6111d9ea228cbd038f7451
Scalro <rotichcaleb200@gmail.com>
2023-04-15 01:43:33 -0700

    Task 0

---
+++ 0x09-static_libraries/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Scalrotech
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "Scalrotech";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
